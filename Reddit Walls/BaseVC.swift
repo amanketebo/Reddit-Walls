0a96a21ecbd911ce6647cc7d684e43e68d1a5907
Aman Ketebo <14272404+amanketebo@users.noreply.github.com>
2017-10-05 16:28:30 -0700

    Use BaseVC to extract same delegate methods
    Both WallpapersVC and FavoritesVC contained collection view
    delegate methods that were exactly the same. The creation of
    BaseVC allows for that code to be in one place.

---
+++ Reddit Walls/BaseVC.swift
@@ -0,0 +1,47 @@
+//
+//  BaseVC.swift
+//  Reddit Walls
+//
+//  Created by Amanuel Ketebo on 10/5/17.
+//  Copyright © 2017 Amanuel Ketebo. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
+extension BaseVC: UICollectionViewDelegate
+{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        let cell = collectionView.cellForItem(at: indexPath)
+        
+        performSegue(withIdentifier: Segue.wallpaper, sender: cell)
+    }
+}
+
+extension BaseVC: UICollectionViewDelegateFlowLayout
+{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let availableWidth = view.bounds.size.width - (Dimension.edgeInsets.left * 2)
+        
+        return CGSize(width: availableWidth, height: Dimension.cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return Dimension.edgeInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize
+    {
+        return CGSize(width: view.bounds.size.width, height: Dimension.footerHeight)
+    }
+}
