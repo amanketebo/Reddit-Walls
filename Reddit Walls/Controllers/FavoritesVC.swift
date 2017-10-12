947dee9e76c7faf2eeba62b5e3c97f0e12a3e1b3
amanketebo <amanuelketebo@yahoo.com>
2017-10-11 22:08:51 -0700

    Add abilty to remove favorites from FavoritesVC

--- Reddit Walls/Controllers/FavoritesVC.swift
+++ Reddit Walls/Controllers/FavoritesVC.swift
@@ -33,6 +33,27 @@ class FavoritesVC: BaseVC
         collectionView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
+    func changeFavoriteStatus(_ sender: UITapGestureRecognizer)
+    {
+        guard let wallpaperCell = sender.view?.superview?.superview as? WallpaperCell else { return }
+        
+        let wallpaperCellTag = wallpaperCell.tag
+        let selectedWallpaper = stuffManager.favorites[wallpaperCellTag]
+        
+        if stuffManager.favoritesContains(selectedWallpaper)
+        {
+            selectedWallpaper.favorite = false
+            stuffManager.removeFavorite(selectedWallpaper)
+        }
+        else
+        {
+            selectedWallpaper.favorite = true
+            stuffManager.favorites.append(selectedWallpaper)
+        }
+        
+        collectionView.reloadData()
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
         if let selectedWallpaperVC = segue.destination as? SelectedWallpaperVC
@@ -69,11 +90,24 @@ extension FavoritesVC: UICollectionViewDataSource {
         if stuffManager.favorites.count != 0
         {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "wallpaperCell", for: indexPath) as! WallpaperCell
+            let wallpaper = stuffManager.favorites[indexPath.row]
             
             cell.tag = indexPath.row
             cell.title.text = stuffManager.favorites[indexPath.row].title
             cell.author.text = stuffManager.favorites[indexPath.row].author
             cell.wallpaper.image = UIImage(named: "gray")!
+            cell.favoriteIcon.image = UIImage(named: "unfilledstar")!
+            cell.favoriteIcon.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(changeFavoriteStatus(_:))))
+            
+            // Set up favorite icon
+            if stuffManager.favoritesContains(wallpaper)
+            {
+                cell.favoriteIcon.image = UIImage(named: "filledstar")!
+            }
+            else
+            {
+                cell.favoriteIcon.image = UIImage(named: "unfilledstar")!
+            }
             
             if let wallpaperURL = URL(string: stuffManager.favorites[indexPath.row].fullResolutionURL)
             {
