f70ea0f4110f669668827400b8736e0b99645c12
Aman Ketebo <14272404+amanketebo@users.noreply.github.com>
2018-04-14 15:39:14 -0700

    Fix opening brace issue

--- Reddit Walls/Models/Wallpaper.swift
+++ Reddit Walls/Models/Wallpaper.swift
@@ -9,66 +9,54 @@
 import SwiftyJSON
 import Foundation
 
-class Wallpaper: Equatable
-{
+class Wallpaper: Equatable {
     static let title = "title"
     static let author = "author"
     static let url = "url"
-    
+
     var title = ""
     var author = ""
     var middleResolutionURL = ""
     var fullResolutionURL = ""
     var favorite = false
-    
+
     init(_ title: String,
          _ author: String,
          _ fullResolutionURL: String,
-         _ favorite: Bool = false)
-    {
+         _ favorite: Bool = false) {
         self.title = title
         self.author = author
         self.fullResolutionURL = fullResolutionURL
         self.favorite = favorite
     }
-    
-    static func == (lhs: Wallpaper, rhs: Wallpaper) -> Bool
-    {
+
+    static func == (lhs: Wallpaper, rhs: Wallpaper) -> Bool {
         if lhs.title == rhs.title &&
             lhs.author == rhs.author &&
             lhs.middleResolutionURL == rhs.middleResolutionURL &&
-            lhs.fullResolutionURL == rhs.fullResolutionURL
-        {
+            lhs.fullResolutionURL == rhs.fullResolutionURL {
             return true
-        }
-        else
-        {
+        } else {
             return false
         }
     }
 }
 
-extension Wallpaper
-{
-    convenience init?(_ json: JSON)
-    {
+extension Wallpaper {
+    convenience init?(_ json: JSON) {
         let title = json[SwiftyJSONPaths.title].stringValue
         let author = json[SwiftyJSONPaths.author].stringValue
         let fullResolutionURL = json[SwiftyJSONPaths.fullResolution].stringValue
-        
+
         self.init(title, author, fullResolutionURL)
     }
-    
-    convenience init?(_ wallpaperInfo: [String: String], favorite: Bool)
-    {
+
+    convenience init?(_ wallpaperInfo: [String: String], favorite: Bool) {
         if let title = wallpaperInfo["title"],
             let author = wallpaperInfo["author"],
-            let fullResolutionURL = wallpaperInfo["url"]
-        {
+            let fullResolutionURL = wallpaperInfo["url"] {
             self.init(title, author, fullResolutionURL, favorite)
-        }
-        else
-        {
+        } else {
             return nil
         }
     }
