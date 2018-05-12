81c87d10c5df01b5e24e98bc15f345e282f68b7d
amanketebo <amanuelketebo@yahoo.com>
2018-05-11 19:14:53 -0700

    Fix caching issue

--- Reddit Walls/Controllers/Parents/BaseVC.swift
+++ Reddit Walls/Controllers/Parents/BaseVC.swift
@@ -48,9 +48,10 @@ class BaseVC: UIViewController {
                 cell.wallpaperHasLoaded = true
             } else {
                 cell.wallpaperHasLoaded = false
-                wallpaperRequester.fetchWallpaperImage(from: wallpaperURL) { (wallpaper, _) in
+                wallpaperRequester.fetchWallpaperImage(from: wallpaperURL) { [weak self] (wallpaper, _) in
                     if cell.tag == indexPath.row {
                         if let wallpaper = wallpaper {
+                            self?.stuffManager.addToCache(wallpaperURL, wallpaper: wallpaper)
                             cell.wallpaper.image = wallpaper
                             cell.wallpaperHasLoaded = true
                         }
